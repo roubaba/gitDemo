1eee6df537b123afdb1f96b24ad8f396306a8d3f
roubaba <roubaba@126.com>
2025-02-26 14:49:24 +0800

    在master分支里面第5次提交代码

--- src/main/java/com/roubaba/other/GitTest.java
+++ src/main/java/com/roubaba/other/GitTest.java
@@ -20,7 +20,7 @@ public class GitTest {
 
     public String eat(String foodName){
         System.out.println("我最喜欢吃辣椒炒肉");
-        return "我最喜欢吃辣椒炒肉";
+        return "我最喜欢吃辣椒炒肉master test !";
     }
 
 
