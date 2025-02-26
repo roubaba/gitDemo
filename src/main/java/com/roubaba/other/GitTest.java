f8e68aff34e71e2403a7f16bd5809d70e895dbf0
roubaba <roubaba@126.com>
2025-02-26 14:14:49 +0800

    在dev分支里面第一次提交代码

--- src/main/java/com/roubaba/other/GitTest.java
+++ src/main/java/com/roubaba/other/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("Hello Git !");
         System.out.println("Hello Git 2 !");
         System.out.println("Hello Git 3 !");
+        System.out.println("Hello Git 3 !" + "这里是dev的分支，请注意");
     }
 }
