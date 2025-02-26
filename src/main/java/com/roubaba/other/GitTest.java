a52bc324d89a047946a5f840f6ce034346939235
roubaba <roubaba@126.com>
2025-02-26 14:22:05 +0800

    在dev分支里面第二次提交代码

--- src/main/java/com/roubaba/other/GitTest.java
+++ src/main/java/com/roubaba/other/GitTest.java
@@ -8,4 +8,12 @@ public class GitTest {
         System.out.println("Hello Git 3 !");
         System.out.println("Hello Git 3 !" + "这里是dev的分支，请注意");
     }
+
+
+    public void sport(String name, int age){
+        System.out.println("我叫" + name + "，我今年" + age + "岁了！");
+    }
+
+
+
 }
